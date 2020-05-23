659e04e141cd677be3cbedccc75e70034bd27ea1
Mark Chenpingling <chenpingling@protonmail.com>
2020-05-23 11:54:18 +0800

    fix

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.vSTOCKCURRENTListBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vSTOCKCURRENTListBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(44, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(161, 70);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Step 1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("PMingLiU", 15F);
+            this.lbl1.Location = new System.Drawing.Point(41, 247);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(187, 25);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "to show some msg";
+            this.lbl1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("DejaVu Sans", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 116);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(1211, 322);
+            this.dataGridView1.TabIndex = 2;
+
+            this.dataGridView1.Anchor = (AnchorStyles.Top | AnchorStyles.Left|AnchorStyles.Bottom|AnchorStyles.Right);
+            // 
+            // vSTOCKCURRENTListBindingSource
+            // 
+            this.vSTOCKCURRENTListBindingSource.DataSource = typeof(WindowsFormsApp7.V_STOCK_CURRENT_List);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vSTOCKCURRENTListBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+       
+        //public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource vSTOCKCURRENTListBindingSource;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            Control control = (Control)sender;
+            //dataGridView1.Width = control.Width - 8;
+           
+            //dataGridView1.Size = new Size(control.Size.Width - 38, dataGridView1.Height);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void show(String info)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+            System.Diagnostics.Debug.WriteLine(info);
+            //Console.WriteLine(info);
+
+        }
+        static void show2(String info)
+        {
+            info += " ";
+            //Console.OutputEncoding = Encoding.UTF8;
+            System.Diagnostics.Debug.Write(info);
+            //Console.Write(info);
+
+        }
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "好久不見";
+
+            var maxRec = 20;
+            using (XUELONGEntities context = new XUELONGEntities())
+            {
+                var obj = context.V_STOCK_CURRENT_List.Where(s => s.cinvcode == "AB");
+                obj.Load();
+                //dataGridView1.DataSource = obj;
+                //dataGridView1.Dock = DockStyle.Fill;
+                //DataAdapter = new
+                //https://stackoverflow.com/questions/42114690/what-is-the-correct-way-to-use-entity-framework-as-datasource-for-datagridview
+                //t will work but the DbContext cannot track changes that way, you should use 
+                
+                context.V_STOCK_CURRENT_List.Where(s => s.cinvcode == "AB").Load(); 
+                dataGridView1.DataSource = context.V_STOCK_CURRENT_List.Local.ToBindingList();
+                dataGridView1.AutoResizeColumns();
+                dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.LightGray;
+                dataGridView1.EnableHeadersVisualStyles = false;
+                dataGridView1.ReadOnly = true;
+                dataGridView1.AllowUserToAddRows = false;
+
+                //dataGridView1.RowTemplate.Resizable = DataGridViewTriState.True;
+
+                //dataGridView1.RowTemplate.Height = 50;
+                foreach(DataGridViewRow row in dataGridView1.Rows )
+                {
+                    row.Height = 35;
+                }
+
+
+
+                var cnt = 0;
+                //foreach (var x in context.V_BASE_CARGOSPACE_L.Where(s => s.cpositioncode == "XL-001"))
+                foreach (var x in context.V_STOCK_CURRENT_List.Where(s => s.cinvcode == "AB"))
+                {
+                    cnt += 1;
+                    if (cnt > maxRec)
+                    {
+                        break;
+                    }
+                    foreach (var col in x.GetType().GetProperties())
+                    {
+                        //show(col.ToString());
+
+                        //show(col.Name.ToString());
+
+                        var v2 = context.Entry(x).Property(col.Name.ToString()).CurrentValue;
+                        if (v2 != null)
+                        {
+
+                            show2(v2.ToString());
+                        }
+
+
+
+
+
+                    }
+
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
